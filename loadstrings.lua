1fa565c75df8166996b1a71276d20c09621e3c6c
seth <107208142+s-eths@users.noreply.github.com>
2025-04-24 23:50:23 +1000

    Create loadstrings.lua

---
+++ loadstrings.lua
@@ -0,0 +1,5 @@
+-- main (works with mobile too)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/s-eths/bubble-gum-simulator-infinity/refs/heads/main/main.lua", true))();
+
+-- xeno fix
+loadstring(game:HttpGet("https://raw.githubusercontent.com/s-eths/bubble-gum-simulator-infinity/refs/heads/main/xeno.lua", true))();
